3b385ef3a978986a10ba69c7d7cbf456e5b1728e
lim-youn-ho <limyounhoo@gmail.com>
2024-02-21 19:17:30 +0900

    -[구글로그인] 백엔드 호출 로직 추가

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ import{
 
 export default function GoogleLoginP(){
     return(
-        <GoogleOAuthProvider clientId="구글에서 발급 받은 클라이언트 아이디 값">
+        <GoogleOAuthProvider clientId="279127607320-dm5sk2f4lna6sv2u713rbtq2o6tc1e2r.apps.googleusercontent.com">
             <GoogleLogin onSuccess={(credentialResponse) => {
                 //response Token
              console.log(credentialResponse);
